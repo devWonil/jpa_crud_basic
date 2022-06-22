a93528a36825d615de1e4525f3a9a459a403326b
devWonil <102580743+devWonil@users.noreply.github.com>
2022-06-22 16:42:15 +0900

    pagination 처리

---
+++ src/main/java/com/tencoding/blog/service/BoardService.java
@@ -0,0 +1,29 @@
+package com.tencoding.blog.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.tencoding.blog.dto.BoardSaveRequestDto;
+import com.tencoding.blog.model.Board;
+import com.tencoding.blog.repository.BoardRepository;
+
+@Service
+public class BoardService {
+
+	@Autowired
+	private BoardRepository boardRepository;
+	
+	@Transactional
+	public void 글쓰기(BoardSaveRequestDto dto) {
+		Board boardEntity = BoardSaveRequestDto.toEntity(dto); 
+		boardRepository.save(boardEntity);
+	}
+	
+	@Transactional
+	public Page<Board> 글목록보기(Pageable pageable) {
+		return boardRepository.findAll(pageable);
+	}
+}
